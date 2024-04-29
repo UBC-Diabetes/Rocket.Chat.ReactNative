af6b7cfebd7dc83215df6f8f2bc3a12f91743eb0
Timothy Roy <timothy@Timothys-MacBook-Pro-2.local>
2024-04-28 21:14:39 -0500

    Add profile button on CalendarView top right

--- app/views/CalendarView/index.tsx
+++ app/views/CalendarView/index.tsx
@@ -3,20 +3,40 @@ import { ScrollView, Text, View, Image } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import Touchable from 'react-native-platform-touchable';
+import { useSelector } from 'react-redux';
+
+import { getUserSelector } from '../../selectors/login';
+import { IApplicationState } from '../../definitions';
 
 import { useTheme } from '../../theme';
 import StatusBar from '../../containers/StatusBar';
+import Avatar from '../../containers/Avatar';
 import * as HeaderButton from '../../containers/HeaderButton';
 
 const CalendarView = (): React.ReactElement => {
     const { colors } = useTheme();
     const navigation = useNavigation<StackNavigationProp<any>>();
+	const user = useSelector((state: IApplicationState) => getUserSelector(state));
+	const userName = user?.username || '';
 
   const [selected, setSelected] = useState('');
 	useEffect(() => {
 		navigation.setOptions({ title: '', headerStyle: { shadowColor: 'transparent' } });
 			navigation.setOptions({
 				headerLeft: () => <HeaderButton.Drawer navigation={navigation} testID='calendar-view-drawer' />,
+		headerRight: () => (
+					<HeaderButton.Container>
+						<Touchable style={{ marginRight: 20 }} onPress={() => navigation.navigate('ProfileView')}>
+							{userName ? (
+								<Avatar text={userName} size={24} borderRadius={12} />
+							) : (
+								<></>
+							)}
+						</Touchable>
+					</HeaderButton.Container>
+				)
+
 			});
 	});
 
