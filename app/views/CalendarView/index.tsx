69c979ee38d8519e527d4372c8f562c4581529de
Timothy Roy <timothy@Timothys-MacBook-Pro-2.local>
2024-04-28 21:03:30 -0500

    Add Hamburger/Sidebar menu button to CalendarView

--- app/views/CalendarView/index.tsx
+++ app/views/CalendarView/index.tsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Text, View, Image } from 'react-native';
 import { Calendar } from 'react-native-calendars';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 import { useTheme } from '../../theme';
+import StatusBar from '../../containers/StatusBar';
+import * as HeaderButton from '../../containers/HeaderButton';
 
 const CalendarView = (): React.ReactElement => {
-	const { colors } = useTheme();
+    const { colors } = useTheme();
+    const navigation = useNavigation<StackNavigationProp<any>>();
 
   const [selected, setSelected] = useState('');
+	useEffect(() => {
+		navigation.setOptions({ title: '', headerStyle: { shadowColor: 'transparent' } });
+			navigation.setOptions({
+				headerLeft: () => <HeaderButton.Drawer navigation={navigation} testID='calendar-view-drawer' />,
+			});
+	});
+
 
   return (
+
+      <ScrollView style={{ flex: 1, padding: 20, backgroundColor: colors.backgroundColor }} testID='calendar-view'>
+          <StatusBar />
     <Calendar
       onDayPress={day => {
         setSelected(day.dateString);
@@ -17,6 +33,7 @@ const CalendarView = (): React.ReactElement => {
         [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
       }}
     />
+          </ScrollView>
   );
 }
 
